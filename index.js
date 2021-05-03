2bb1c842affd5095fc9721cad50884b372e08106
Myles Starr <mylesstarr@gmail.com>
2021-05-02 22:15:43 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("text") 
+    console.log(paragraph)
+    paragraph.textContent ="This is really cool!"
+    }
+)
